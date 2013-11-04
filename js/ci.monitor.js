a7d1f4e549442dd0157fb8d30325ba2cdf5e9df7
Ming Jin <skyairmj@gmail.com>
2013-11-04 17:17:45 +0800

    Ming Jin: in progress

--- js/ci.monitor.js
+++ js/ci.monitor.js
@@ -1,14 +1,10 @@
 onmessage = function(event) {
     var data = event.data;
-    persist(data);
     updateUI(data);
-}
-
-function persist(data) {
-    
 }
 
 function updateUI(data)
 {
-    postMessage([{id: 1, state: data.result.toLowerCase()}, {id: 2, state: 'failed'}]);
+    postMessage(data);
+    // postMessage([{id: 1, state: data.result.toLowerCase()}, {id: 2, state: 'failed'}]);
 }
